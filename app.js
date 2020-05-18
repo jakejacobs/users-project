973573c8ae24ad45dc1e384047f60b2f30613a59
Jake Jacobs <jacky4uonly2009@gmail.com>
2020-05-18 22:29:38 +0530

    added get,post, patch, delete methods

--- app.js
+++ app.js
@@ -1,33 +1,119 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
 const fs = require('fs');
 
 const app = express()
-
-const publicDirectoryPath = path.join(__dirname,'./public')
-app.use(express.static(publicDirectoryPath))
-
-
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
+app.use(express.json())
 
-app.get('/api/users/:id', (req,res)=>{
-   
-        try {
-          const data = fs.readFileSync(path.join(__dirname, './api/users.json'));
-          const users = JSON.parse(data);
-          const userName = users.find(user => user.username.toLowerCase() === String(req.params.id.toLowerCase()));
-          if (!userName) {
-            res.json({"error":"Username not found"})
+const users = JSON.parse(fs.readFileSync(`${__dirname}/api/users.json`))
+
+//GET users
+app.get('/api/v1/users',(req,res)=>{
+  res.status(200).json({
+      success:200,
+      results:users.length,
+      data:{
+          users:users
+      }
+  })
+})
+
+//GET single user
+app.get('/api/v1/users/:username',(req,res)=>{
+
+  const findUser = users.find((user)=>{
+      return user.username.toLowerCase() === String(req.params.username.toLowerCase())
+  })
+  if(!findUser){
+      return res.status(404).json({
+          status:"fail",
+          message:"Invalid Username"
+      })
+  } 
+      
+      res.status(200).json({
+          success:200,
+          data:{
+              user:findUser
           }
-          res.json(userName);
-        } catch (e) {
-          console.log(e);
           
-        }
-});
+      })
+})
+
+//POST
+app.post('/api/v1/users',(req,res)=>{
+  const newid = users[users.length - 1].id + 1
+  
+  const newUser = Object.assign({
+      id:newid,
+  },req.body)
+  users.push(newUser)
+ fs.writeFile(`${__dirname}/api/users.json`,JSON.stringify(users),(err)=>{
+     res.status(201).json({
+         status:"success",
+         data:{
+             user:newUser
+         }
+     })
+ })
+})
+
+//Update Post
+app.patch('/api/v1/users/:username',(req,res)=>{
+  let updateUser = users.find((user)=>{
+      return user.username.toLowerCase() === String(req.params.username.toLowerCase())
+  })
+
+  if(!updateUser){
+      return res.status(404).json({
+          status:"fail",
+          message:"Invalid Username"
+      })
+  }
+  updateUser = {
+      id:updateUser.id,
+      name:req.body.name,
+      username:req.body.username,
+      email:req.body.email
+      
+  }
+
+  users.splice(parseInt(updateUser.id - 1),1,updateUser)
+  fs.writeFile(`${__dirname}/api/users.json`,JSON.stringify(users),(err)=>{
+      res.status(201).json({
+          status:"success",
+          data:{
+              user:updateUser
+          }
+      })
+  })
+  
+})
+
+//Delete user
+
+app.delete('/api/v1/users/:username',(req,res)=>{
+  let deleteUser = users.find((user)=>{
+      return user.username.toLowerCase() === String(req.params.username.toLowerCase())
+  })
+
+  if(!deleteUser){
+      return res.status(404).json({
+          status:"fail",
+          message:"Invalid Username"
+      })
+  }
+
+  users.splice(parseInt(deleteUser.id - 1),1)
+  fs.writeFile(`${__dirname}/api/users.json`,JSON.stringify(users),(err)=>{
+      res.status(204).json({
+          status:"success",
+          data:null
+      })
+  })
+  res.send("User deleted successfully")
+})
 
 //npm start
 app.listen(PORT,()=>{
